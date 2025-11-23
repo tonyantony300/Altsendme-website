7cc3a420cb2324fc647733b2305f8a6bb030ba0c
tony <tonyantony300@gmail.com>
2025-11-23 22:48:25 +0530

    chore: bump version v0.2.4

--- src/constants/downloadOptions.js
+++ src/constants/downloadOptions.js
@@ -1,26 +1,28 @@
 // Single source of truth for download options
 // Labels are added dynamically using translations in components
 
+const VERSION = "0.2.4";
+
 export const downloadOptionsData = [
   {
     id: "windows",
     size: "8 MB",
     icon: "/windows.svg",
-    url: "https://github.com/tonyantony300/alt-sendme/releases/download/v0.2.3/AltSendme_0.2.3_x64-setup.exe",
+    url: "https://github.com/tonyantony300/alt-sendme/releases/download/v" + VERSION + "/AltSendme_" + VERSION + "_x64-setup.exe",
     translationKey: "hero.downloadForWindows",
   },
   {
     id: "mac",
     size: "26 MB",
     icon: "/applelogo.svg",
-    url: "https://github.com/tonyantony300/alt-sendme/releases/download/v0.2.3/AltSendme_0.2.3_universal.dmg",
+    url: "https://github.com/tonyantony300/alt-sendme/releases/download/v" + VERSION + "/AltSendme_" + VERSION + "_universal.dmg",
     translationKey: "hero.downloadForMac",
   },
   {
     id: "linux-appimage",
     size: "87 MB",
     icon: "/linuxlogo.svg",
-    url: "https://github.com/tonyantony300/alt-sendme/releases/download/v0.2.3/AltSendme_0.2.3_amd64.AppImage",
+    url: "https://github.com/tonyantony300/alt-sendme/releases/download/v" + VERSION + "/AltSendme_" + VERSION + "_amd64.AppImage",
     translationKey: "hero.downloadForLinux",
   },
 ];
