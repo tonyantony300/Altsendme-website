148058f0f02d53cae54c4c9c52bf8497d2a0c3ee
tony <tonyantony300@gmail.com>
2026-01-25 20:26:36 +0530

    update version

--- src/constants/downloadOptions.js
+++ src/constants/downloadOptions.js
@@ -1,7 +1,7 @@
 // Single source of truth for download options
 // Labels are added dynamically using translations in components
 
-const VERSION = "0.2.4";
+const VERSION = "0.3.0";
 
 export const downloadOptionsData = [
   {
